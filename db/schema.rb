d79a56a073965b5cdf838790f5a6c594c4d2a09d
Elton Santos <eltonaxl@hotmail.com>
2023-06-19 01:01:46 -0300

    Create medicines, symptoms, medicine_symptoms and active storage

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,67 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_18_045028) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_19_040050) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "active_storage_attachments", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
+  create_table "medicine_symptoms", force: :cascade do |t|
+    t.bigint "medicine_id", null: false
+    t.bigint "symptom_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["medicine_id"], name: "index_medicine_symptoms_on_medicine_id"
+    t.index ["symptom_id"], name: "index_medicine_symptoms_on_symptom_id"
+  end
+
+  create_table "medicines", force: :cascade do |t|
+    t.string "name"
+    t.integer "quantity"
+    t.date "medicine_validity"
+    t.text "medicine_insert"
+    t.string "used_to"
+    t.date "purchase_date"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_medicines_on_user_id"
+  end
+
+  create_table "symptoms", force: :cascade do |t|
+    t.string "name"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_symptoms_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -28,4 +86,10 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_18_045028) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "medicine_symptoms", "medicines"
+  add_foreign_key "medicine_symptoms", "symptoms"
+  add_foreign_key "medicines", "users"
+  add_foreign_key "symptoms", "users"
 end
