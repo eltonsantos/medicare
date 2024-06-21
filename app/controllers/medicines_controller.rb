c36ddcc9d50d2fa2595f4179e44c3978a7c7312b
Elton Santos <eltonaxl@hotmail.com>
2024-06-21 08:21:56 -0300

    Initial commit with all configured

---
+++ app/controllers/medicines_controller.rb
@@ -0,0 +1,58 @@
+class MedicinesController < ApplicationController
+  before_action :set_medicine, only: %i[ show edit update destroy ]
+
+  # GET /medicines
+  def index
+    @medicines = Medicine.all
+  end
+
+  # GET /medicines/1
+  def show
+  end
+
+  # GET /medicines/new
+  def new
+    @medicine = Medicine.new
+  end
+
+  # GET /medicines/1/edit
+  def edit
+  end
+
+  # POST /medicines
+  def create
+    @medicine = Medicine.new(medicine_params)
+
+    if @medicine.save
+      redirect_to @medicine, notice: "Medicine was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  # PATCH/PUT /medicines/1
+  def update
+    if @medicine.update(medicine_params)
+      redirect_to @medicine, notice: "Medicine was successfully updated.", status: :see_other
+    else
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+  # DELETE /medicines/1
+  def destroy
+    @medicine.destroy!
+    redirect_to medicines_url, notice: "Medicine was successfully destroyed.", status: :see_other
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_medicine
+      @medicine = Medicine.find(params[:id])
+    end
+
+    # Only allow a list of trusted parameters through.
+    def medicine_params
+      params.require(:medicine).permit(:name, :quantity, :medicine_validity, :medicine_insert, :used_to, :purchase_date, :user_id)
+    end
+end
---
+++ db/migrate/20240621110829_add_roles_and_name_to_user.rb
@@ -0,0 +1,6 @@
+class AddRolesAndNameToUser < ActiveRecord::Migration[7.1]
+  def change
+    add_column :users, :role, :integer
+    add_column :users, :name, :string
+  end
+end
