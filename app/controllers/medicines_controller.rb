29aa99b6213bf9ae937721fd035621ba28349ef5
Elton Santos <eltonaxl@hotmail.com>
2024-06-25 00:50:17 -0300

    Add and adjustments modal on symptom

--- app/controllers/medicines_controller.rb
+++ app/controllers/medicines_controller.rb
@@ -2,29 +2,20 @@ class MedicinesController < ApplicationController
   before_action :set_medicine, only: %i[ show edit update destroy ]
   before_action :set_paper_trail_whodunnit
 
-  # GET /medicines
   def index
     @medicines = Medicine.all
   end
 
-  # GET /medicines/1
   def show
-    respond_to do |format|
-      format.html { render(:show) }
-      format.turbo_stream { render(:show) }
-    end
   end
 
-  # GET /medicines/new
   def new
     @medicine = Medicine.new
   end
 
-  # GET /medicines/1/edit
   def edit
   end
 
-  # POST /medicines
   def create
     @medicine = current_user.medicines.new(medicine_params)
 
@@ -35,7 +26,6 @@ class MedicinesController < ApplicationController
     end
   end
 
-  # PATCH/PUT /medicines/1
   def update
     if @medicine.update(medicine_params)
       redirect_to medicines_url, notice: "Medicine was successfully updated.", status: :see_other
@@ -44,7 +34,6 @@ class MedicinesController < ApplicationController
     end
   end
 
-  # DELETE /medicines/1
   def destroy
     @medicine.destroy!
     redirect_to medicines_url, notice: "Medicine was successfully destroyed.", status: :see_other
@@ -59,12 +48,10 @@ class MedicinesController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_medicine
       @medicine = Medicine.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def medicine_params
       params.require(:medicine).permit(:name, :unit, :is_liquid, :quantity, :description, :medicine_validity, :medicine_insert, :used_to, :user_id, symptom_ids: [])
     end
--- app/controllers/symptoms_controller.rb
+++ app/controllers/symptoms_controller.rb
@@ -1,57 +1,48 @@
 class SymptomsController < ApplicationController
   before_action :set_symptom, only: %i[ show edit update destroy ]
 
-  # GET /symptoms
   def index
     @symptoms = Symptom.all
   end
 
-  # GET /symptoms/1
   def show
   end
 
-  # GET /symptoms/new
   def new
     @symptom = Symptom.new
   end
 
-  # GET /symptoms/1/edit
   def edit
   end
 
-  # POST /symptoms
   def create
     @symptom = Symptom.new(symptom_params)
 
     if @symptom.save
-      redirect_to @symptom, notice: "Symptom was successfully created."
+      redirect_to symptoms_url, notice: "Symptom was successfully created."
     else
       render :new, status: :unprocessable_entity
     end
   end
 
-  # PATCH/PUT /symptoms/1
   def update
     if @symptom.update(symptom_params)
-      redirect_to @symptom, notice: "Symptom was successfully updated.", status: :see_other
+      redirect_to symptoms_url, notice: "Symptom was successfully updated.", status: :see_other
     else
       render :edit, status: :unprocessable_entity
     end
   end
 
-  # DELETE /symptoms/1
   def destroy
     @symptom.destroy!
     redirect_to symptoms_url, notice: "Symptom was successfully destroyed.", status: :see_other
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
     def set_symptom
       @symptom = Symptom.find(params[:id])
     end
 
-    # Only allow a list of trusted parameters through.
     def symptom_params
       params.require(:symptom).permit(:name, :description)
     end
