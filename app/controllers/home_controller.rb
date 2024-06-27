a73db5fe893b3ff0db9ac3de5b3649fd8fe81ab9
Elton Santos <eltonaxl@hotmail.com>
2024-06-27 03:39:37 -0300

    Add home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,11 @@
 class HomeController < ApplicationController
   def index
+    if current_user.role != "admin"
+      @medicine = Medicine.where(user_id: current_user.id)
+      @symptom = Symptom.all
+    else
+      @medicine = Medicine.all
+      @symptom = Symptom.all
+    end
   end
 end
