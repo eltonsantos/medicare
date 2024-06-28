9d2aec082223c8c062dd1a0ad9c08bd39b9d0f9a
Elton Santos <eltonaxl@hotmail.com>
2024-06-28 15:24:55 -0300

    Add expiring medicines on home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,9 +3,11 @@ class HomeController < ApplicationController
     if current_user.role != "admin"
       @medicine = Medicine.where(user_id: current_user.id)
       @symptom = Symptom.all
+      @expiring_medicines = current_user.medicines.where("medicine_validity <= ?", 10.days.from_now).order(:medicine_validity)
     else
       @medicine = Medicine.all
       @symptom = Symptom.all
+      @expiring_medicines = Medicine.where("medicine_validity <= ?", 10.days.from_now).order(:medicine_validity)
     end
   end
 end
