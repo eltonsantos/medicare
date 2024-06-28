8329afdeca881ef232d1161feac2bf25742d508c
Elton Santos <eltonaxl@hotmail.com>
2024-06-28 14:58:08 -0300

    Can attach medicine insert and download it

--- app/models/medicine.rb
+++ app/models/medicine.rb
@@ -7,6 +7,19 @@ class Medicine < ApplicationRecord
   has_many :symptoms, through: :medicine_symptoms
   
   has_one_attached :picture
+  has_one_attached :medicine_insert
 
   validates :name, :unit, :quantity, :medicine_validity, presence: true
+  validate :validate_medicine_insert_attachment
+
+  private
+
+  def validate_medicine_insert_attachment
+    if medicine_insert.attached?
+      unless medicine_insert.content_type.in?(%w(application/pdf))
+        errors.add(:medicine_insert, 'Deve ser em formato PDF')
+        medicine_insert.purge
+      end
+    end
+  end
 end
