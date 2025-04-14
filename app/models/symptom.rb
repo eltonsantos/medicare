577f90759b5c7f235a02b1ad1b338feb0297fe54
Elton Santos <eltonaxl@hotmail.com>
2025-04-14 03:10:23 +0100

    Remove comments and change aspect color scroll vertical

--- app/models/symptom.rb
+++ app/models/symptom.rb
@@ -1,16 +1,12 @@
 class Symptom < ApplicationRecord
-  # Associations
   has_many :medicine_symptoms, dependent: :destroy
   has_many :medicines, through: :medicine_symptoms
 
-  # Validations
   validates :name, presence: true, length: { minimum: 2, maximum: 50 }, uniqueness: { case_sensitive: false }
   
-  # Scopes
   scope :ordered, -> { order(name: :asc) }
   scope :with_medicines, -> { joins(:medicines).distinct }
   
-  # Callbacks
   before_save :capitalize_name
   
   private
